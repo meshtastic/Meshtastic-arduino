aebe920a8ee09349408e38ec3a40c8fc6e82df9f
Todd Herbert <herbert.todd@gmail.com>
2024-11-10 22:59:43 +1300

    Update GitHub action Bundles NanoPB Bumps NanoPB to 0.4.9 Remove device only proto, to avoid std::vector on ESP8266

--- bin/regen-protos.sh
+++ bin/regen-protos.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-echo "This script requires https://jpa.kapsi.fi/nanopb/download/ version 0.4.7 to be located in the"
+echo "This script requires https://jpa.kapsi.fi/nanopb/download/ version 0.4.9 to be located in the"
 echo "firmware root directory if the following step fails, you should download the correct"
-echo "prebuilt binaries for your computer into nanopb-0.4.7"
+echo "prebuilt binaries for your computer into nanopb-0.4.9"
 
 # the nanopb tool seems to require that the .options file be in the current directory!
 cd protobufs
-../nanopb-0.4.7/generator-bin/protoc --nanopb_out=-v:../src -I=../protobufs meshtastic/*.proto
+../nanopb-0.4.9/generator-bin/protoc --experimental_allow_proto3_optional "--nanopb_out=-S.c -v:../src/" -I=../protobufs meshtastic/*.proto
+
+# remove the device only protobuf, so we don't need std::vector
+rm -rf ../src/meshtastic/deviceonly.*
 
 #echo "Regenerating protobuf documentation - if you see an error message"
 #echo "you can ignore it unless doing a new protobuf release to github."
